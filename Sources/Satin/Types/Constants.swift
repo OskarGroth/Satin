321845c71ddf0747c80f0c440591e9264bf333ca
Oskar Groth <oskar@cindori.org>
2022-02-07 00:51:24 +0100

    change world up

--- Sources/Satin/Types/Constants.swift
+++ Sources/Satin/Types/Constants.swift
@@ -11,7 +11,7 @@ import simd
 public let maxBuffersInFlight: Int = 3
 
 public let worldForwardDirection = simd_make_float3(0, 0, 1)
-public let worldUpDirection = simd_make_float3(0, 1, 0)
+public let worldUpDirection = simd_make_float3(0, -1, 0)
 public let worldRightDirection = simd_make_float3(1, 0, 0)
 
 public enum Blending {
